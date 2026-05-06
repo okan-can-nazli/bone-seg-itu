2d806f56b3c0f3c4267b1e15cd1fad589e0ae0c2
okan-can-nazli <okancanazli@gmail.com>
2026-05-06 23:26:21 +0300

    bug fix on dataset.py

--- dataset.py
+++ dataset.py
@@ -78,13 +78,13 @@ def build_file_lists(images_dir, masks_dir):
     
     # sorted() provides index matching between iimages and masks (based on index)
     
-    for sub_folder in sorted(os.listdir(images_dir), key=lambda x: int(x)):
+    for sub_folder in sorted([f for f in os.listdir(images_dir) if os.path.isdir(os.path.join(images_dir, f))], key=lambda x: int(x)):
         sub_path = os.path.join(images_dir, sub_folder)
         for f in os.listdir(sub_path):
             if f.lower().endswith(".jpg"):
                 images_ls.append(os.path.join(sub_path, f))
                 
-    for sub_folder in sorted(os.listdir(masks_dir), key=lambda x: int(x)):
+    for sub_folder in sorted([f for f in os.listdir(masks_dir) if os.path.isdir(os.path.join(masks_dir, f))], key=lambda x: int(x)):
         sub_path = os.path.join(masks_dir, sub_folder)
         masks_ls.append(sub_path)
     
