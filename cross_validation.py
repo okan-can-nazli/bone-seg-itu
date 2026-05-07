0646d5d975772c90a917bc13051c9212146adc27
okan-can-nazli <okancanazli@gmail.com>
2026-05-07 07:55:58 +0300

    add visualation lines in main.py

--- cross_validation.py
+++ cross_validation.py
@@ -26,10 +26,11 @@ def main():
     # LOCAL
     # IMAGE_DIR = "Data/images"
     # MASK_DIR = "Data/masks"
-
+    IMAGE_DIR = "/content/New_Labels"
+    MASK_DIR = "/content/New_masks"
     # COLAB
-    IMAGE_DIR = "/content/drive/MyDrive/Bone/New_Labels"
-    MASK_DIR = "/content/drive/MyDrive/Bone/New_masks"
+    #IMAGE_DIR = "/content/drive/MyDrive/Bone/New_Labels"
+    #MASK_DIR = "/content/drive/MyDrive/Bone/New_masks"
     OUTPUT_DIR = "/content/drive/MyDrive/Bone/bone_seg_outputs"
     
     os.makedirs(OUTPUT_DIR, exist_ok=True)
