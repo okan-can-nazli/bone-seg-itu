561d8dfd60fff332ca7224d26f4f6fc5fd8dba5a
okan-can-nazli <okancanazli@gmail.com>
2026-05-07 15:09:46 +0300

    kaggle fix

--- cross_validation.py
+++ cross_validation.py
@@ -28,7 +28,7 @@ def main():
     # MASK_DIR = "Data/masks"
     IMAGE_DIR = "/kaggle/input/datasets/okancannazli/bones-seg/New_Labels-20260504T191710Z-3-001/New_Labels"
     MASK_DIR  = "/kaggle/input/datasets/okancannazli/bones-seg/New_masks-20260504T191902Z-3-001/New_masks"
-    OUTPUT_DIR = "/kaggle/working/outputs""
+    OUTPUT_DIR = "/kaggle/working/outputs"
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     image_paths, mask_folders = build_file_lists(IMAGE_DIR, MASK_DIR)
