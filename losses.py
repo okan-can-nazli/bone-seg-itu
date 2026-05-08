79ae3b68fa8d8a67f95c6004b9cd45e41a30aabc
okan-can-nazli <okancanazli@gmail.com>
2026-05-08 23:15:08 +0300

    update comments

--- losses.py
+++ losses.py
@@ -6,14 +6,14 @@
 import torch
 import torch.nn as nn
 
-
+# (1-dice_score) loss calculation based on overall image (explorer)
 def dice_loss(pred, target, smooth=1e-9): # smooth: prevents 0/0 condition
     pred = torch.sigmoid(pred)  # logits → probabilities (0-1)
     intersection = torch.sum(pred * target)
     dice = (2 * intersection + smooth) / (torch.sum(pred) + torch.sum(target) + smooth)
     return 1 - dice 
 
-
+# loss calculation based on pixel by pixel (stabilizer)
 def bce_dice_loss(pred, target, bce_weight=0.5):
     bce = nn.BCEWithLogitsLoss()(pred, target.float())
     dice = dice_loss(pred, target)
