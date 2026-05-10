2a43dca518eb47519a4ad1b76c2e4bf9ed2b36d6
okan-can-nazli <okancanazli@gmail.com>
2026-05-10 14:34:31 +0300

    change default execution directories as local

--- cross_validation.py
+++ cross_validation.py
@@ -26,14 +26,14 @@ BATCH_SIZE = 8
 #! Directory
 
 # LOCAL DIRS
-# IMAGE_DIR = "Data/images"
-# MASK_DIR = "Data/masks"
-#OUTPUT_DIR = "outputs"
+IMAGE_DIR = "Data/images"
+MASK_DIR = "Data/masks"
+OUTPUT_DIR = "outputs"
 
 #KAGGLE DIRS
-IMAGE_DIR = "/kaggle/input/datasets/okancannazli/bones-seg/New_Labels-20260504T191710Z-3-001/New_Labels"
-MASK_DIR  = "/kaggle/input/datasets/okancannazli/bones-seg/New_masks-20260504T191902Z-3-001/New_masks"
-OUTPUT_DIR = "/kaggle/working/outputs"
+# IMAGE_DIR = "/kaggle/input/datasets/okancannazli/bones-seg/New_Labels-20260504T191710Z-3-001/New_Labels"
+# MASK_DIR  = "/kaggle/input/datasets/okancannazli/bones-seg/New_masks-20260504T191902Z-3-001/New_masks"
+# OUTPUT_DIR = "/kaggle/working/outputs"
 #########################################################################################################
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
